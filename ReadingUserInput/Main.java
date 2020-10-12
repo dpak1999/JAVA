d38b840ef3ffb3f8fe3b27f6d4498eda5e73cd28
Deepak Kumar <55288948+dpak1999@users.noreply.github.com>
2020-10-12 16:09:04 +0530

    Update Main.java

--- ReadingUserInput/Main.java
+++ ReadingUserInput/Main.java
@@ -18,3 +18,11 @@ public class Main {
         scanner.close();
     }
 }
+//nextBoolean() 	Reads a boolean value from the user
+//nextByte() 	Reads a byte value from the user
+//nextDouble() 	Reads a double value from the user
+//nextFloat() 	Reads a float value from the user
+//nextInt() 	Reads a int value from the user
+//nextLine() 	Reads a String value from the user
+//nextLong() 	Reads a long value from the user
+//nextShort() 	Reads a short value from the user
